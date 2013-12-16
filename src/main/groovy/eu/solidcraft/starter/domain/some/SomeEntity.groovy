865b3eacca2f94088919f5b6e711b167619d04f6
Jakub Nabrdalik <jakubn@gmail.com>
2013-12-16 08:55:29 +0100

    schema changes

--- src/main/groovy/eu/solidcraft/starter/domain/some/SomeEntity.groovy
+++ src/main/groovy/eu/solidcraft/starter/domain/some/SomeEntity.groovy
@@ -1,6 +1,9 @@
 package eu.solidcraft.starter.domain.some
 
-import javax.persistence.*
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.SequenceGenerator
 import javax.validation.constraints.NotNull
 
 @Entity
