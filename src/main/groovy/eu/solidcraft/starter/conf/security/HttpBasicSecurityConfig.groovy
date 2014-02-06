4a178467e0e8159a02c3d2920f1c206fcc91378e
Jakub Nabrdalik <jakubn@gmail.com>
2014-02-06 11:12:53 +0100

    csrf disabled

--- src/main/groovy/eu/solidcraft/starter/conf/security/HttpBasicSecurityConfig.groovy
+++ src/main/groovy/eu/solidcraft/starter/conf/security/HttpBasicSecurityConfig.groovy
@@ -30,6 +30,7 @@ class HttpBasicSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.
+            csrf().disable().
             authorizeRequests().
                  anyRequest().authenticated().
                  and().
